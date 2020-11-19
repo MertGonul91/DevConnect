c3f4f7ed89b70f859dd18640e86fa091bf842017
MertGonul91 <mertgonul91@gmail.com>
2020-11-19 16:11:52 +0100

    mongoose fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
+const User = require('../../models/User');
+
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
@@ -15,13 +17,33 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // See if user exists
+
+      // Get users gravatar
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+
+      res.send('User route');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
