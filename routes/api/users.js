7df754639eecacd8a4e54493a0bc1aa1966189a9
MertGonul91 <mertgonul91@gmail.com>
2020-11-19 15:38:29 +0100

    Second Commit

---
+++ routes/api/users.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
+router.post(
+  '/',
+  [
+    body('name', 'Name is required').not().isEmpty(),
+    body('email', 'Please include a valid email').isEmail(),
+    body(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('User route');
+  }
+);
+
+module.exports = router;
