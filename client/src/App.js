0c983bc4b1342189da540720fbddb8630da9064f
MertGonul91 <mertgonul91@gmail.com>
2020-11-20 23:57:06 +0100

    Navbar and Landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
 
 const App = () => (
   <>
-    <h1>APP</h1>
+    <Navbar />
+    <Landing />
   </>
 );
 
