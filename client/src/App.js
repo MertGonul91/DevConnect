de6fdaddef90bf5aa8abe92f6aa14c50e8aaa896
MertGonul91 <mertgonul91@gmail.com>
2020-11-22 16:37:31 +0100

    Added links to routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => (
@@ -9,6 +11,12 @@ const App = () => (
     <>
       <Navbar />
       <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </>
   </Router>
 );
